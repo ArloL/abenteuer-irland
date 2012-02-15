21fb1d65c8b69754ad56752dc782994fb4ea596c
Arlo O'Keeffe <MailToArlo@googlemail.com>
2012-02-15 23:45:11 +0100

    Updated to h5bp 3.0

--- index.php
+++ index.php
@@ -3,16 +3,16 @@
 	$aPage = getConfig();
 ?><!doctype html>
 <!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
-<!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="<?php echo $aPage['content-language'] ?>"> <![endif]-->
-<!--[if IE 7]>		<html class="no-js ie7 oldie" lang="<?php echo $aPage['content-language'] ?>"> <![endif]-->
-<!--[if IE 8]>		<html class="no-js ie8 oldie" lang="<?php echo $aPage['content-language'] ?>"> <![endif]-->
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="<?php echo $aPage['content-language'] ?>"> <![endif]-->
+<!--[if IE 7]>		<html class="no-js lt-ie9 lt-ie8 ie7" lang="<?php echo $aPage['content-language'] ?>"> <![endif]-->
+<!--[if IE 8]>		<html class="no-js lt-ie9" lang="<?php echo $aPage['content-language'] ?>"> <![endif]-->
 <!-- Consider adding an manifest.appcache: h5bp.com/d/Offline -->
 <!--[if gt IE 8]><!--> <html class="no-js" lang="<?php echo $aPage['content-language'] ?>"> <!--<![endif]-->
 <head>
 	<meta charset="utf-8">
 
 	<!-- Use the .htaccess and remove these lines to avoid edge case issues.
-			 More info: h5bp.com/b/378 -->
+			 More info: h5bp.com/i/378 -->
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
 	<title><?php echo $aPage['title'] ?></title>
@@ -20,7 +20,7 @@
 	<meta name="description" content="<?php echo $aPage['description']; ?>" />
 	<meta name="author" content="Mary O'Keeffe">
 
-	<!-- Mobile viewport optimized: j.mp/bplateviewport -->
+	<!-- Mobile viewport optimized: h5bp.com/viewport -->
 	<meta name="viewport" content="width=device-width,initial-scale=1">
 
 	<base href="<?php echo $aPage['base']; ?>">
@@ -34,10 +34,10 @@
 
 	<!-- More ideas for your <head> here: h5bp.com/d/head-Tips -->
 
-	<!-- All JavaScript at the bottom, except for Modernizr / Respond.
-			 Modernizr enables HTML5 elements & feature detects; Respond is a polyfill for min/max-width CSS3 Media Queries
-			 For optimal performance, use a custom Modernizr build: www.modernizr.com/download/ -->
-	<script src="js/libs/modernizr.custom.96237.js"></script>
+	<!-- All JavaScript at the bottom, except this Modernizr build.
+			 Modernizr enables HTML5 elements & feature detects for optimal performance.
+			 Create your own custom Modernizr build: www.modernizr.com/download/ -->
+	<script src="js/libs/modernizr.custom.82050.js"></script>
 </head>
 
 <body id="<?php echo $aPage['css-id']?>">
@@ -46,7 +46,7 @@
 	<div id="center">
 		<div id="container">
 			<header>
-				<a class="logo" href="." title="Abenteuer Irland"><img src="img/logo.png" width="316" height="70" title="Abenteuer Irland" alt="Abenteuer Irland"></a>
+				<a class="logo ir" href="." title="Abenteuer Irland">Abenteuer Irland</a>
 				<div id="telBox">
 					<p>
 						<a href="mailto:mok@abenteuer-irland.de">mok@abenteuer-irland.de</a><br />
@@ -78,11 +78,13 @@
 		</div> <!--! end of #container -->
 	</div> <!--! end of #center -->
 
+	<!-- Asynchronous Google Analytics snippet. Change UA-XXXXX-X to be your site's ID.
+       mathiasbynens.be/notes/async-analytics-snippet -->
 	<script>
-		window._gaq = [['_setAccount','<?php echo $aPage['analytics-id']?>'],['_trackPageview'],['_trackPageLoadTime']];
-		Modernizr.load({
-			load: ('https:' == location.protocol ? '//ssl' : '//www') + '.google-analytics.com/ga.js'
-		});
+	    var _gaq=[['_setAccount','<?php echo $aPage['analytics-id']?>'],['_trackPageview']];
+	    (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+	    g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+	    s.parentNode.insertBefore(g,s)}(document,'script'));
 	</script>
 </body>
 </html>

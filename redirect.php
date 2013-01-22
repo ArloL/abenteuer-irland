8279d0e66ac7ef5fef784dc8171b848bed96394e
Arlo O'Keeffe <mailtoarlo@gmail.com>
2013-01-22 14:58:00 +0100

    Show 404 page if not exists

--- redirect.php
+++ redirect.php
@@ -16,4 +16,7 @@ if (array_key_exists($sPath, $aRedirects)) {
 	$sRedirect = $aRedirects[$sPath];
 	header('HTTP/1.1 301 Moved Permanently');
 	header('Location: ' . $sRedirect);
+} else {
+	header("HTTP/1.1 404 Not Found");
+	include '404.html';
 }

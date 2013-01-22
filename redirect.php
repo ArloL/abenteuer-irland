232db68b7dc212bda0a1002443e3a7276ba826d4
Arlo O'Keeffe <mailtoarlo@gmail.com>
2013-01-22 14:53:34 +0100

    Added redirect.php

---
+++ redirect.php
@@ -0,0 +1,19 @@
+---
+layout: nil
+---
+<?php
+
+$aRedirects = array(
+    'home' => '{{site.url}}',
+    'en/home' => '{{site.url}}/en/',
+    'english' => 'http://business-english-bocholt.de',
+    'englisch' => 'http://business-english-bocholt.de'
+);
+
+$sPath = $_GET['path'];
+
+if (array_key_exists($sPath, $aRedirects)) {
+	$sRedirect = $aRedirects[$sPath];
+	header('HTTP/1.1 301 Moved Permanently');
+	header('Location: ' . $sRedirect);
+}
